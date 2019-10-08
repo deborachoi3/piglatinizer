925bc14d6b0037e4cfcc24851a113ba2a06b5cac
deborachoi3 <bbybokchoi@gmail.com>
2019-10-08 01:07:29 +0000

    piglatin

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $( "document" ).ready(function() {
-
-
-
+$("button").click(function() {
+    let word = $(".input").val();
+    let firstLetter = word[0]
+    //if (firstLetter == "a" || firstLetter == "i" || firstLetter == "e" || firstLetter == "o"|| firstLetter == "u")
+    let pigLatin = word + "ay";
+   console.log (pigLatin)
+    $(".output").text(pigLatin);
+    let firstConsonant = word [0]
+    //if (firstConsonant != "a" || firstConsonant != "i" || firstConsonant != "e" || firstConsonant != "o"|| firstConsonant != "u")
+    let pigLatin = word + firstConsonant + "ay";
+  console.log (pigLatin)
+});
 });
